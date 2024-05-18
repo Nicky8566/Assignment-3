1d8bb46bc2fc13b57f444296cf6ecbc618892ced
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-19 03:16:38 +1200

    read the contries data and made it into a data structure. I presume I have done it right, will have to see

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,9 +1,16 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  private String country;
+  private String continent;
+  private String taxFees;
+  private ArrayList<String> info;
+  private Map<String, List<String>> countiresInfo;
 
   public MapEngine() {
     // add other code here if you want
@@ -15,6 +22,21 @@ public class MapEngine {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
+    // intiate new hashmap
+
+    for (String c : countries) {
+      // split the sountry string when theres a , into 3 parts
+      String[] parts = c.split(",");
+      // create a new country object with the 3 parts
+      country = parts[0];
+      continent = parts[1];
+      taxFees = parts[2];
+      info = new ArrayList<String>();
+      info.add(continent);
+      info.add(taxFees);
+      // add the country object to the hashmap
+      countiresInfo.put(country, info);
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
