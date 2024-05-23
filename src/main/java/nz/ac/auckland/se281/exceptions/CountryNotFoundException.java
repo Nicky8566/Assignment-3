4bfc8360c1cba6e97a571fdcb7184a0ff0983939
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-23 19:42:43 +1200

    name changed

--- src/main/java/nz/ac/auckland/se281/exceptions/CountryNotFoundException.java
+++ src/main/java/nz/ac/auckland/se281/exceptions/CountryNotFoundException.java
@@ -1,4 +1,4 @@
-package nz.ac.auckland.se281.temp_exceptions;
+package nz.ac.auckland.se281.exceptions;
 
 public class CountryNotFoundException extends Exception {
   public CountryNotFoundException(String message) {
